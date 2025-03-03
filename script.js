f4d6a3071431725b123821b56d692ade0f2e915a
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-03-04 02:04:40 +0530

    hex code is use for color changer

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let box = document.getElementById('colorbox')
 let start = document.getElementById('start')
 let stop = document.getElementById('stop')
 
+
+let randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)]
+        
+    } return color
+}
+
+console.log(randomColor());
+
+
 function changecolor(){
     console.log("color are changing")
 }
