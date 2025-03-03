23dbfa909c98a3b1d8b1239f48986b05ffc9492a
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-03-04 01:38:28 +0530

    added trial and basic function

---
+++ script.js
@@ -0,0 +1,10 @@
+let box = document.getElementById('colorbox')
+let start = document.getElementById('start')
+let stop = document.getElementById('stop')
+
+function changecolor(){
+    console.log("color are changing")
+}
+function colorStop(){
+    console.log("stopped")
+}
